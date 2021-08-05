2b4f11358307f0ecc3f0e37121c9f0477a71771e
NayanSidhu <nsidh068@uottawa.ca>
2021-08-04 23:29:18 -0400

    fixed routing of html

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,12 +2,12 @@ const path = require('path');
 const router = require('express').Router(); 
 
 //Returns the Index HTML page routing to the root 
-router('/', (req, res) => {
+router.get('/', (req, res) => {
     res.sendFile(path.json(__dirname, '../../public/index.html'));
 }); 
 
 //Returns the ADD HTML page routing 
-router('/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     res.sendFile(path.json(__dirname, '../../public/notes.html'));
 });
 
